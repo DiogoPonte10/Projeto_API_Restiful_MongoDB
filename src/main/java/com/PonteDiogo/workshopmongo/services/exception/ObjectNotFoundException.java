e060f5ac069ce4885069db93a886a4d4cabe154e
Diogo Ponte <diogo_ponte20@hotmail.com>
2024-08-27 20:20:36 -0300

    Obtendo usuário por id

---
+++ src/main/java/com/PonteDiogo/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.PonteDiogo.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
